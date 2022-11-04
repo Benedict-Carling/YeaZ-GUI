f672e6972487d66d0930de600969506ef60dfb62
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2022-11-04 08:44:50 +0000

    chore: Add images

--- equalise_exposure.py
+++ equalise_exposure.py
@@ -7,7 +7,7 @@ from unet.segment import segment
 
 DATAPATH = "example_data"
 OUTPUTPATH = "output_data"
-FILEPATH = "channel2_time0.tif"
+FILEPATH = "channel0_fov0_Z2_time0-3.tif"
 # FILEPATH = "2020_3_19_frame_100_cropped.tif"
 
 
